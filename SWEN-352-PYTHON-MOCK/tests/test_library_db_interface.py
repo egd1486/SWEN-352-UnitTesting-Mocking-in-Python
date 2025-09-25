ab487a65f075c88483409ed020e179c8b90ae4ce
bab8295 <bab8295@g.rit.edu>
2025-09-25 15:28:31 -0400

    added comments, is 100%

--- SWEN-352-PYTHON-MOCK/tests/test_library_db_interface.py
+++ SWEN-352-PYTHON-MOCK/tests/test_library_db_interface.py
@@ -26,17 +26,24 @@ class TestLibbraryDBInterface(unittest.TestCase):
 
     def test_insert_patron_false_patron(self):
         patron_mock = MagicMock()
+        # Does a check to see if the patron is None or False, can't set __bool__ to none
+        # If patron is none or false returns none, so asserting that
         patron_mock.__bool__.return_value = False
         self.assertIsNone(self.db_interface.insert_patron(patron_mock))
 
     def test_get_patron_count(self):
         results_mock = MagicMock()
+        # get patron count returns lenght of results,
+        # so just setting length and mocking the "all" call that
+        # then has len() called on the results of
         results_mock.__len__.return_value = 10
         self.db_interface.db.all = Mock(return_value=results_mock)
         self.assertEqual(self.db_interface.get_patron_count(), 10)
 
     def test_get_all_patrons(self):
-        patron_mock = Mock()        
+        patron_mock = Mock()
+        # mocking the outside api call that grabs patrons,
+        # makes sure that the get_all_patrons returns what the api returns
         self.db_interface.db.all = Mock(return_value=[patron_mock])        
         self.assertEqual(self.db_interface.get_all_patrons(), [patron_mock])
 
@@ -51,15 +58,19 @@ class TestLibbraryDBInterface(unittest.TestCase):
 
     def test_update_patron_false(self):
         patron_mock = MagicMock()
+        # making sure that if given false or None patron
+        # that None is returned by check
         patron_mock.__bool__.return_value = False
         self.assertIsNone(self.db_interface.update_patron(patron_mock))
 
     def test_retrieve_patron_data_compare(self):
+        # retrieving a patron and making sure the data is the same
         data = [{'fname': 'name', 'lname': 'name', 'age': '2', 'memberID': '3',
                 'borrowed_books': []}]
         self.db_interface.db.search = Mock(return_value=data)
         mock_member_id = Mock()
         retrieved: patron.Patron = self.db_interface.retrieve_patron(mock_member_id)
+
 
         self.assertEqual(retrieved.fname, data[0]['fname'])
         self.assertEqual(retrieved.lname, data[0]['lname'])
@@ -68,6 +79,10 @@ class TestLibbraryDBInterface(unittest.TestCase):
         self.assertEqual(retrieved.borrowed_books, data[0]['borrowed_books'])
 
     def test_retrieve_patron_object_compare(self):
+        # retreiving a patron and making sure that
+        # the data is the same when __eq__ is called
+        # thereby checking __dict__ of patrons
+        # effectively makes sure patron is constructed properly
         data = [{'fname': 'name', 'lname': 'name', 'age': '2', 'memberID': '3',
                 'borrowed_books': []}]
         self.db_interface.db.search = Mock(return_value=data)
@@ -83,6 +98,8 @@ class TestLibbraryDBInterface(unittest.TestCase):
 
 
     def test_retrieve_patron_false(self):
+        # Making sure that if search returns False / None
+        # this returns None
         self.db_interface.db.search = Mock(return_value=False)
         self.assertIsNone(self.db_interface.retrieve_patron(Mock()))
 
